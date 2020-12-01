ea9a2bd5ad1d5702333da89c2df542b1fbfbca54
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-12-01 17:39:40 +0100

    prova arrow function e concatenazione

--- snackObject3/js/script.js
+++ snackObject3/js/script.js
@@ -2,18 +2,28 @@
 // numeri (a più piccolo di b). La funzione ritornerà un nuovo
 // array con i valori che hanno la posizione compresa tra i due
 // numeri
-var mainArray = ['ciao', 'prova', 'penna', 'divano', 'letto', 'casa', 'cane'];
-var a = 1;
-var b = 3;
-
-var prova = nonLoSo(mainArray, a, b);
-console.log(prova);
+/*-----------------------------------ES6 function--------------------------------*/
+const nonLoSo2 = (array, nMin, nMax) => {
+  let newArray = [];
+  for (let i = nMin; i <= nMax; i++){
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
 
 /*------------------FUNZIONI----------------------------------------*/
 function nonLoSo(array, nMin , nMax) {
   var newArray = [];
   for (var i = nMin; i <= nMax; i++) {
-    newArray.push(array[i])
+    newArray.push(array[i]);
   }
   return newArray;
 }
+
+
+var mainArray = ['ciao', 'prova', 'penna', 'divano', 'letto', 'casa', 'cane'];
+var a = 1;
+var b = 3;
+
+var prova = nonLoSo2(mainArray, a, b);
+console.log(prova);
---
+++ test-es6/js/script.js
@@ -0,0 +1,37 @@
+/*  <==== rimuovi qui
+
+//1. INCATENAZIONE DI PIÙ STRINGHE
+
+let frase1 = 'ciao';
+let frase2 = 'come';
+let frase3 = 'stai?';
+
+// concatenazione di più stringhe
+console.log(frase1 + ' ' + frase2 + ' ' + frase3 + '\nbene grazie');
+
+// concatenazione di più stringhe es6 utilizzo `` e le variabili
+// precedute da $ e coprese tra {}
+// il bene grazie non necessita di apici o virgolette
+console.log(`${frase1}, ${frase2}, ${frase3}, \nbene grazie`);
+
+let printHtml = `
+<h1>Titolo</h1>
+<br>
+<p>${frase1} ${frase2} ${frase3}</p>
+`;
+document.getElementById('incatenazione-es-1').innerHTML = printHtml;
+
+rimuovi qui ====> */
+
+//2. ARROW FUNCTION
+
+// fuzione senza argomenti
+const myFunction = () => 1 + 5;
+let result = myFunction()
+// equivalente scritto nel 'vecchio metodo'
+/* function myFuntion() {return 1 + 5};  */
+console.log(result);
+
+
+/*  <==== rimuovi qui
+rimuovi qui ====> */
